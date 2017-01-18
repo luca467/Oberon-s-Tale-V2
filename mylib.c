8b1ca8482a77da1cf1d17f21027e904583668fab
luca467 <luca467@gmail.com>
2017-01-18 17:18:52 +0100

    Add files via upload

---
+++ mylib.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "mylib.h"
+
+static struct Terra *head = NULL;
+static struct Terra *lastland = NULL;
+
+void crea_percorso(){
+  int t,x=0;
+	
+  do{
+    printf("Cosa vuoi fare?\n");      
+    printf("1)Inserisci Terra\n");
+    printf("2)Cancella Terra\n");
+    printf("3)Stampa Percorso\n");
+    printf("4)Torna indietro\n");
+
+    scanf("%d", &t);
+
+    switch (t){
+
+      case 1:{
+	ins_terra();
+	break;
+      }
+
+      case 2:{
+        canc_terra();
+        break;
+      }
+
+      case 3:{
+	stampa_percorso();
+	break;
+      }
+
+      case 4:{
+	x++;
+	break;
+      }
+
+      default:{
+        printf("Hai inserito un comando sbagliato .-.\n");
+        break;
+      }
+    };
+  }while(x==0);
+}
+
+void ins_terra(){
+	
+  int tt, tm,x=0;
+  short int ts;		
+				
+  do{
+    printf("Che Terra vuoi inserire?\n");
+    printf("0)Deserto\n");
+    printf("1)Foresta\n");
+    printf("2)Palude\n");
+    printf("3)Villaggio\n");
+    printf("4)Pianura\n");
+    scanf("%d", &tt);
+    if(tt>4){
+      printf("Hai sbagliato comando\n");
+    }
+    else{
+      x++;
+    }
+  }while(x==0);
+  x=0;
+  
+  if(head!=NULL){
+    if(tt!=3){
+      do{
+	printf("Che mostro vuoi inserire?\n");
+	printf("0)Nessuno\n");
+	printf("1)Scheletro\n");
+	printf("2)Lupo\n");
+	printf("3)Orco\n");
+	printf("4)Drago\n");
+	
+	scanf("%d", &tm);
+	if(tm>4){
+	  printf("Hai sbagliato comando\n");
+	}
+	else if(tm==1 && tt==2){
+	  printf("Lo scheletro non può stare su una palude\n");
+	}
+	else if(tm==3 && tt==0){
+	  printf("L'orco non può stare su un deserto\n");
+	}
+	else if((tm==2 && tt!=1)||(tm==2 && tt!=4)){
+	  printf("Il lupo può stare solo sulla foresta o sulla pianura\n");
+	}
+	else{
+	  x++;
+	}
+      }while(x==0);
+    }
+    else{
+      tm=0;
+    }
+  }
+  else{
+    tm=0;
+  }
+  x=0;
+  do{
+    printf("Quanto oro vuoi inserire in questa terra? N.B. Il massimo è 200, per i villaggi 10\n");
+    scanf("%hd", &ts);
+    if(ts>10 && tt==3){
+      printf("Il tesoro è troppo grande\n"); 
+    }
+    else if(ts>200){
+      printf("Il tesoro è troppo grande!\n");
+    }
+    else{
+      x++;
+    }
+  }while(x==0);			
+  x=0;
+  
+  if(head==NULL){
+    head =  malloc(sizeof(struct Terra));
+    head->Tipo_terra = tt;
+    head->Tipo_mostro= tm;
+    head->tesoro = ts;
+    head->next = NULL;
+  }
+  else{	
+    struct Terra *lastland = head;
+    while(lastland->next != NULL){
+      lastland = lastland ->next;
+    }
+    lastland->next = malloc(sizeof(struct Terra));	
+    lastland->next->Tipo_terra = tt;
+    lastland->next->Tipo_mostro= tm;
+    lastland->next->tesoro = ts;
+    lastland->next->next = NULL;
+  }
+}
+
+void canc_terra(){
+	
+  if(head == NULL){
+    printf("Non hai inserito ancora nessuna Terra.\n");
+  }
+  
+  if(head->next == NULL){
+    free(head);
+  }
+
+  else{
+    struct Terra *current = head;
+	
+    while(current->next->next != NULL){ //arriva in fondo alla lista
+      current = current->next;
+    }
+
+    free(current->next);
+    current->next = NULL;
+  }
+}
+
+
+void stampa_percorso(){
+	
+  struct Terra *pp = head;
+  
+  if(pp == NULL){
+    printf("Non hai creato nessuna Terra\n");
+  }
+  
+  else{
+    do{
+      printf("\n%d, %d, %hd\n\n", pp->Tipo_terra, pp->Tipo_mostro, pp->tesoro);
+      pp = pp->next;
+    }while(pp != NULL);
+  } 
+}
+
+void termina_gioco(){
+  
+  struct Terra *current = head;
+  
+  if(current!=NULL){
+    do{
+      //free(head);
+      current = current->next;
+      printf("canc\n");
+    }while(current != NULL);
+  }
+  
+}
---
+++ progexam.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "mylib.h"
+
+int main(){
+
+  
+  
+  int a;
+  int b=0;
+  do{
+
+    printf(" ___________________________________________________________\n");		
+    printf("|----------------MAGO OBERON CALZINO BUCATO-----------------|\n");
+    printf("|In questo gioco si dovrà condurre il Mago Oberon attraverso|\n");
+    printf("|varie terre, con lo scopo di farlo arrivare sano e salvo a |\n");
+    printf("|casa.                                                      |\n");
+    printf("|___________________________________________________________|\n");
+    printf("\n");
+    printf(" _________________________________________________\n");
+    printf("|                                                 |\n");
+    printf("| COSA VUOI FARE?                                 |\n");
+    printf("| (Digita 1, 2, o 3 a seconda dell'opzione scelta)|\n");
+    printf("|                                                 |\n");
+    printf("| 1)Crea Percorso.                                |\n");
+    printf("|                                                 |\n");
+    printf("| 2)Muovi Oberon.                                 |\n");
+    printf("|                                                 |\n");
+    printf("| 3)Termina Gioco.                                |\n");
+    printf("|_________________________________________________|\n");
+    
+    scanf("%d", &a);
+    switch (a){
+      
+      case 1:{
+        crea_percorso();
+        break;
+      }
+      
+      case 2:{
+        printf("Appena implementerò la funzione, muoverai Oberon.\n");
+        break;
+      }
+      
+      case 3:{
+        printf("Cattivone! Te ne vai di già... :(\n");
+	termina_gioco();
+	b++;
+        break;
+      }
+      
+      default:{
+        printf("ERROR: Scelta non vailda.\n");
+      }
+    }
+  }while(b==0);
+  
+  return 0;
+}
